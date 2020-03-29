64cda452088cd3fbe2237426a25f3cfe5047ffbc
ganesh96-lab <54699093+ganesh96-lab@users.noreply.github.com>
2020-03-29 07:59:23 -0400

    Update EmpWageComputation.java

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,22 +1,18 @@
 class EmpWageComputation
 {
 	//CONSTANTS
-		public static final int IS_FULL_TIME=1;
-		public static final int IS_PART_TIME=0;
-		public static final int EMP_RATE_PER_HRS=20;
-				
-
+	public static final int IS_FULL_TIME=1;
+	public static final int IS_PART_TIME=0;
+	public static final int EMP_RATE_PER_HRS=20;
+		
 	public static void main(String args[])
 	{
 		System.out.println("Welcome to Employee Wage Computation");
-		
-		
+	
 		//variables
 		int empHrs=0;
 		int empWage=0;
-
-		int empCheck=((int)Math.floor(Math.random()*10))%2; 
-		
+		int empCheck=((int)Math.floor(Math.random()*10))%2;	
 		switch(empCheck){
 			case IS_FULL_TIME:
 				empHrs=8;
@@ -29,6 +25,5 @@ class EmpWageComputation
 		}
 		empWage=empHrs*EMP_RATE_PER_HRS;
 		System.out.println("Emp Wage for "+empHrs+" hrs : "+empWage);
-
 	}
 }

a81fda6ac9a0c9d30eca613cc99a49794e435256
ganesh96-lab <ganeshghodke7050@gmail.com>
2020-03-29 05:11:01 -0400

    added part time and full time work wages

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -6,7 +6,7 @@ class EmpWageComputation
 		//CONSTANTS
 		int IS_FULL_TIME=1;
 		int EMP_RATE_PER_HRS=20;
-
+		int IS_PART_TIME=0;
 		//variables
 		int empHrs=0;
 		int empWage=0;
@@ -16,12 +16,16 @@ class EmpWageComputation
 		{
 			empHrs=8;
 		}
+		else if(empCheck==IS_PART_TIME)
+		{
+			empHrs=4;
+		}
 		else
 		{
 			empHrs=0;
 		}
 		empWage=empHrs*EMP_RATE_PER_HRS;
-		System.out.println("Emp Wage : "+empWage);
+		System.out.println("Emp Wage for "+empHrs+" hrs : "+empWage);
 
 	}
 }

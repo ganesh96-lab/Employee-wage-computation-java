ce348dcee852d9b1e816621c622cfb035f2efe54
ganesh96-lab <ganeshghodke7050@gmail.com>
2020-03-29 04:31:22 -0400

    added uc2 calculate daily wages

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -6,14 +6,23 @@ class EmpWageComputation
 		System.out.println("Welcome to Employee Wage Computation");
 		//CONSTANTS
 		int IS_FULL_TIME=1;
-		double empCheck=((int)Math.floor(Math.random()*10))%2; 
-		if (empCheck==IS_FULL_TIME) 
+		int EMP_RATE_PER_HRS=20;
+
+		//variables
+		int empHrs=0;
+		int empWage=0;
+
+		int empCheck=((int)Math.floor(Math.random()*10))%2; 
+		if(empCheck==IS_FULL_TIME) 
 		{
-			System.out.println("Employee is present");
+			empHrs=8;
 		}
 		else
 		{
-			System.out.println("Employee is absent");
+			empHrs=0;
 		}
+		empWage=empHrs*EMP_RATE_PER_HRS;
+		System.out.println("Emp Wage : "+empWage);
+
 	}
 }

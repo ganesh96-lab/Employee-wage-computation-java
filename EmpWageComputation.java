8e5513e5e93735763777d1714e7fa859fe0eb22b
ganesh96-lab <ganeshghodke7050@gmail.com>
2020-03-29 07:52:25 -0400

    emp wages using method

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -6,20 +6,18 @@ class EmpWageComputation
 		public static final int EMP_RATE_PER_HRS=20;
 		public static final int NUM_WORKING_DAYS=20;	
 		public static final int MAX_HRS_IN_MONTH=100;
-	public static void main(String args[])
+	public void calEmpWages()
 	{
-		System.out.println("Welcome to Employee Wage Computation");
-		
-		
 		//variables
 		int empHrs=0,empWage=0,totalWorkingDays=0,
 			totalEmpWage=0,totalEmpHrs=0;
 
-		while (totalEmpHrs <= MAX_HRS_IN_MONTH &&
-		       totalWorkingDays < NUM_WORKING_DAYS) {
+		while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_WORKING_DAYS) 
+		{
 			totalWorkingDays++;
 			int empCheck = ((int) Math.floor(Math.random() * 10)) % 3;
-			switch(empCheck) {
+			switch(empCheck) 
+			{
 				case IS_FULL_TIME:
 					empHrs = 8;
 					break;
@@ -36,6 +34,12 @@ class EmpWageComputation
  		System.out.println("Employee Wage for month: " + totalEmpWage);
 	}
 
+	public static void main(String args[])
+	{
+		System.out.println("Welcome to Employee Wage Computation");
+		EmpWageComputation cmp=new EmpWageComputation();
+		cmp.calEmpWages();
 
+	}
 	
 }

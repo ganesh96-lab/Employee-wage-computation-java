083c730cc68dc77379312a6d267f56369181e73d
ganesh96-lab <ganeshghodke7050@gmail.com>
2020-03-29 06:35:53 -0400

    ADDED:wages for month

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -4,7 +4,7 @@ class EmpWageComputation
 		public static final int IS_FULL_TIME=1;
 		public static final int IS_PART_TIME=0;
 		public static final int EMP_RATE_PER_HRS=20;
-				
+		public static final int No_OF_WORKING_HRS=2;	
 
 	public static void main(String args[])
 	{
@@ -15,20 +15,25 @@ class EmpWageComputation
 		int empHrs=0;
 		int empWage=0;
 
-		int empCheck=((int)Math.floor(Math.random()*10))%2; 
+		for(int day=1;day<=No_OF_WORKING_HRS;day++)
+		{
+			int empCheck=((int)Math.floor(Math.random()*10))%2; 
 		
-		switch(empCheck){
-			case IS_FULL_TIME:
-				empHrs=8;
-				break;
-			case IS_PART_TIME:
-				empHrs=4;
-				break;
-			default:
-				empHrs=0;
+			switch(empCheck)
+			{ 
+			
+				case IS_FULL_TIME:
+					empHrs=8;
+					break;
+				case IS_PART_TIME:
+					empHrs=4;
+					break;
+				default:
+					empHrs=0;
+			}      
+			empWage=empWage+empHrs*EMP_RATE_PER_HRS;
+		
 		}
-		empWage=empHrs*EMP_RATE_PER_HRS;
-		System.out.println("Emp Wage for "+empHrs+" hrs : "+empWage);
-
+		System.out.println("Wages for month:"+empWage);
 	}
 }

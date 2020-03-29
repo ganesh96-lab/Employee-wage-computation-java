0cdea30eff4963116efce026b9778f65943410b2
ganesh96-lab <ganeshghodke7050@gmail.com>
2020-03-29 07:33:34 -0400

    Calculate Wages till

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -4,36 +4,38 @@ class EmpWageComputation
 		public static final int IS_FULL_TIME=1;
 		public static final int IS_PART_TIME=0;
 		public static final int EMP_RATE_PER_HRS=20;
-		public static final int No_OF_WORKING_HRS=2;	
-
+		public static final int NUM_WORKING_DAYS=20;	
+		public static final int MAX_HRS_IN_MONTH=100;
 	public static void main(String args[])
 	{
 		System.out.println("Welcome to Employee Wage Computation");
 		
 		
 		//variables
-		int empHrs=0;
-		int empWage=0;
+		int empHrs=0,empWage=0,totalWorkingDays=0,
+			totalEmpWage=0,totalEmpHrs=0;
 
-		for(int day=1;day<=No_OF_WORKING_HRS;day++)
-		{
-			int empCheck=((int)Math.floor(Math.random()*10))%2; 
-		
-			switch(empCheck)
-			{ 
-			
+		while (totalEmpHrs <= MAX_HRS_IN_MONTH &&
+		       totalWorkingDays < NUM_WORKING_DAYS) {
+			totalWorkingDays++;
+			int empCheck = ((int) Math.floor(Math.random() * 10)) % 3;
+			switch(empCheck) {
 				case IS_FULL_TIME:
-					empHrs=8;
+					empHrs = 8;
 					break;
 				case IS_PART_TIME:
-					empHrs=4;
+					empHrs = 4;
 					break;
 				default:
-					empHrs=0;
-			}      
-			empWage=empWage+empHrs*EMP_RATE_PER_HRS;
-		
+					empHrs = 0;
+			}
+			totalEmpHrs += empHrs;
+			empWage = empHrs * EMP_RATE_PER_HRS;
+			totalEmpWage += empWage;
 		}
-		System.out.println("Wages for month:"+empWage);
+ 		System.out.println("Employee Wage for month: " + totalEmpWage);
 	}
+
+
+	
 }

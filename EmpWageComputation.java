3e2b89ec198cec4fd8946d3213935eb913d55f5a
ganesh96-lab <ganeshghodke7050@gmail.com>
2020-03-29 03:50:09 -0400

    added uc1 employee is present or absent

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -4,5 +4,16 @@ class EmpWageComputation
 	public static void main(String args[])
 	{
 		System.out.println("Welcome to Employee Wage Computation");
+		//CONSTANTS
+		int IS_FULL_TIME=1;
+		double empCheck=((int)Math.floor(Math.random()*10))%2; 
+		if (empCheck==IS_FULL_TIME) 
+		{
+			System.out.println("Employee is present");
+		}
+		else
+		{
+			System.out.println("Employee is absent");
+		}
 	}
 }

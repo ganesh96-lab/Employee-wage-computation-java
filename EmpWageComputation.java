1c5b2246980515428fea2b089964682b90c9a64b
ganesh96-lab <ganeshghodke7050@gmail.com>
2020-03-31 10:06:29 -0400

    Refactor to have one emp wage builder to manage for wage for multiple company

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,45 +1,20 @@
-public class EmpWageComputation {
+public class EmpWageComputation 
+{
 	//CONSTANTS
-	public static final int IS_PART_TIME = 0;
-	public static final int IS_FULL_TIME = 1;
-	public static int EMP_RATE_PER_HRS;
-	public static int MAX_WORKING_DAYS;
-	public static int MAX_HRS_IN_MONTH;
-	public static int TOTAL_EMP_WAGE;
-
-	//CONSTRUCTOR
-	public EmpWageComputation(int EMP_RATE_PER_HRS, int MAX_WORKING_DAYS, int MAX_HRS_IN_MONTH )	
-	{
-		this.EMP_RATE_PER_HRS = EMP_RATE_PER_HRS;
-		this.MAX_WORKING_DAYS = MAX_WORKING_DAYS;
-		this.MAX_HRS_IN_MONTH = MAX_HRS_IN_MONTH;
-	}
-	//getter methods
-	public static int getEmpRatePerHrs()
-	{
-		return EMP_RATE_PER_HRS;
-	}
-
-	public static int getMaxWorkingDays()
-	{
-		return MAX_WORKING_DAYS;
-	}
-
-	public static int getMaxHrsInMonth()
-	{
-		return MAX_HRS_IN_MONTH;
-	}
-
-	public static int calEmpWages()
+	public final int IS_PART_TIME = 0;
+	public final int IS_FULL_TIME = 1;
+	
+	public void calEmpWages(CompanyEmpWage company)
 	{
 		//variables
 		int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
 
-		while( totalEmpHrs <= getMaxHrsInMonth() &&
-				totalWorkingDays < getMaxWorkingDays() )  {
+		while( totalEmpHrs <= company.getMaxHrsInMonth() && totalWorkingDays < company.getMaxWorkingDays() )
+		 {
 			totalWorkingDays++;
 			int empCheck = (int) ( Math.random() * 10 ) % 3;
-			switch (empCheck) {
+			switch (empCheck)
+			{
 				case IS_PART_TIME:
 					empHrs = 4;
 					break;
@@ -55,17 +30,63 @@ public class EmpWageComputation {
 			System.out.println("Day: " + totalWorkingDays + " Emp Hr: " +  empHrs);
 		}
 		// calculate total Wages
-		TOTAL_EMP_WAGE = totalEmpHrs * getEmpRatePerHrs();
-		return TOTAL_EMP_WAGE;	
+		company.setTotalEmpWage(totalEmpHrs * company.getEmpRatePerHrs());
+		System.out.println("Total Wage of " + company.getCompanyName() + " Employee is " + company.getTotalEmpWage());	
 	}
 
 	public static void main(String args[])
 	{
-		EmpWageComputation bridgelabz = new EmpWageComputation(20, 20, 100 );
-		int wage1 = bridgelabz.calEmpWages();
-		System.out.println("TOTAL EMP WAGE FOR BRIDGELABZ : "+wage1);
-		EmpWageComputation capgminai= new EmpWageComputation(50, 15, 75 );
-		int wage2=capgminai.calEmpWages();
-		System.out.println("TOTAL EMP WAGE FOR CAPGMINI : "+wage2);
+		EmpWageComputation empwagecomputation = new EmpWageComputation();
+		CompanyEmpWage[] companys=new CompanyEmpWage[2];
+
+		companys[0] = new CompanyEmpWage("BRIDGELABZ",20,20,100);
+		empwagecomputation.calEmpWages(companys[0]);
+		
+		companys[1]= new CompanyEmpWage("CAPGMINI",50, 15, 100 );
+		empwagecomputation.calEmpWages(companys[1]);	
+	}
+}
+
+class CompanyEmpWage
+{
+	public int EMP_RATE_PER_HRS;
+	public int MAX_WORKING_DAYS;
+	public int MAX_HRS_IN_MONTH;
+	public String COMPANY_NAME;
+	
+	public int totalEmpWage=0;
+	
+	public CompanyEmpWage(String COMPANY_NAME,int EMP_RATE_PER_HRS, int MAX_WORKING_DAYS, int MAX_HRS_IN_MONTH)
+	{
+		this.EMP_RATE_PER_HRS = EMP_RATE_PER_HRS;
+		this.MAX_WORKING_DAYS = MAX_WORKING_DAYS;
+		this.MAX_HRS_IN_MONTH = MAX_HRS_IN_MONTH;
+		this.COMPANY_NAME=COMPANY_NAME;
+	}
+	//getter methods
+	public int getEmpRatePerHrs()
+	{
+		return EMP_RATE_PER_HRS;
+	}
+
+	public int getMaxWorkingDays()
+	{
+		return MAX_WORKING_DAYS;
+	}
+
+	public int getMaxHrsInMonth()
+	{
+		return MAX_HRS_IN_MONTH;
+	}
+	public String getCompanyName()	{
+		return COMPANY_NAME;
+	}
+	public void setTotalEmpWage(int totalEmpWage)
+	{
+		this.totalEmpWage=totalEmpWage;
+	}
+	public int getTotalEmpWage()
+	{
+		return totalEmpWage;
 	}
 }

cd101961b191e0ee04924d696a7adc3a5b40dc25
ganesh96-lab <ganeshghodke7050@gmail.com>
2020-03-31 06:51:07 -0400

    created emp wage builder for each company

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,58 +1,70 @@
-class EmpWageComputation
-{
+public class EmpWageComputation {
 	//CONSTANTS
-	public static final int IS_FULL_TIME=1;
-	public static final int IS_PART_TIME=0;
-	public final int EMP_RATE_PER_HRS;
-	public final int NUM_WORKING_DAYS;	
-	public final int MAX_HRS_IN_MONTH;
+	public static final int IS_PART_TIME = 0;
+	public static final int IS_FULL_TIME = 1;
+	public static int EMP_RATE_PER_HRS;
+	public static int MAX_WORKING_DAYS;
+	public static int MAX_HRS_IN_MONTH;
+	public static int TOTAL_EMP_WAGE;
 
-	public EmpWageComputation(int EMP_RATE_PER_HRS,int NUM_WORKING_DAYS,int MAX_HRS_IN_MONTH)
+	//CONSTRUCTOR
+	public EmpWageComputation(int EMP_RATE_PER_HRS, int MAX_WORKING_DAYS, int MAX_HRS_IN_MONTH )	
 	{
-		this.EMP_RATE_PER_HRS=EMP_RATE_PER_HRS;
-		this.NUM_WORKING_DAYS=NUM_WORKING_DAYS;
-		this.MAX_HRS_IN_MONTH=MAX_HRS_IN_MONTH;
+		this.EMP_RATE_PER_HRS = EMP_RATE_PER_HRS;
+		this.MAX_WORKING_DAYS = MAX_WORKING_DAYS;
+		this.MAX_HRS_IN_MONTH = MAX_HRS_IN_MONTH;
 	}
-	public int calEmpWages()
+	//getter methods
+	public static int getEmpRatePerHrs()
 	{
-		//variables
-		int empHrs=0,empWage=0,totalWorkingDays=0,
-			totalEmpWage=0,totalEmpHrs=0;
+		return EMP_RATE_PER_HRS;
+	}
 
-		while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_WORKING_DAYS) 
-		{
+	public static int getMaxWorkingDays()
+	{
+		return MAX_WORKING_DAYS;
+	}
+
+	public static int getMaxHrsInMonth()
+	{
+		return MAX_HRS_IN_MONTH;
+	}
+
+	//COMPUTATION
+	public static int calEmpWages()
+	{
+		int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
+		while( totalEmpHrs <= getMaxHrsInMonth() &&
+				totalWorkingDays < getMaxWorkingDays() )  {
 			totalWorkingDays++;
-			int empCheck = ((int) Math.floor(Math.random() * 10)) % 3;
-			switch(empCheck) 
-			{
-				case IS_FULL_TIME:
-					empHrs = 8;
-					break;
+			int empCheck = (int) ( Math.random() * 10 ) % 3;
+			switch (empCheck) {
 				case IS_PART_TIME:
 					empHrs = 4;
+					break;
+				case IS_FULL_TIME:
+					empHrs = 8;
 					break;
 				default:
 					empHrs = 0;
 			}
+			//calculate total emp hrs
 			totalEmpHrs += empHrs;
-			empWage = empHrs * EMP_RATE_PER_HRS;
-			totalEmpWage += empWage;
+			//print daily many work hours 
+			System.out.println("Day: " + totalWorkingDays + " Emp Hr: " +  empHrs);
 		}
- 		return totalEmpWage;
+		// calculate total Wages
+		TOTAL_EMP_WAGE = totalEmpHrs * getEmpRatePerHrs();
+		return TOTAL_EMP_WAGE;	
 	}
-	public static void main(String args[])
-	{ 
-		System.out.println("Welcome to Employee Wage Computation");
-		EmpWageComputation bridgelabz=new EmpWageComputation(20,20,100);
-		EmpWageComputation capgemini=new EmpWageComputation(20,20,100);
-		EmpWageComputation accenture=new EmpWageComputation(20,20,100);
-		int totalEmpWageBridze = bridgelabz.calEmpWages();
-		System.out.println("Emp Wage for bridgelab:"+totalEmpWageBridze);
-		
-		int totalEmpWageCap = capgemini.calEmpWages();
-		System.out.println("Emp wage for capgemini:"+totalEmpWageCap);
 
-		int totalEmpWageAcc=accenture.calEmpWages();
-		System.out.println("Emp wage for capgemini:"+totalEmpWageAcc);
+	public static void main(String args[])
+	{
+		EmpWageComputation bridgelabz = new EmpWageComputation(20, 20, 100 );
+		int wage1 = bridgelabz.calEmpWages();
+		System.out.println("TOTAL EMP WAGE FOR BRIDGELABZ : "+wage1);
+		EmpWageComputation capgminai= new EmpWageComputation(50, 15, 75 );
+		int wage2=capgminai.calEmpWages();
+		System.out.println("TOTAL EMP WAGE FOR CAPGMINI : "+wage2);
 	}
 }
